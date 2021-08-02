1e6b57308c4aae1feba7e198ee0c1c538976ecf5
ctroy978 <ctroy978@gmail.com>
2021-08-02 14:33:04 -0700

    refactored ball rs

--- src/ball.rs
+++ src/ball.rs
@@ -1,6 +1,9 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite::collide_aabb::{collide, Collision},
+};
 
-use crate::{Ball, Materials};
+use crate::{Ball, Materials, Collider, FallingToSpawn};
 
 
 pub struct BallPlugin;
@@ -12,7 +15,8 @@ impl Plugin for BallPlugin{
                 "balls",
                 SystemStage::single(ball_spawn.system()),   
                   )
-            .add_system(ball_movement_system.system());
+            .add_system(ball_movement_system.system())
+            .add_system(ball_collision_system.system());
     }
 }
 
@@ -45,4 +49,67 @@ fn ball_movement_system(
         transform.translation += ball.velocity * delta_seconds;
     }
 }
+
+fn ball_collision_system(
+    mut commands: Commands,
+    mut ball_query: Query<(&mut Ball, &Transform, &Sprite)>,
+    collider_query: Query<(Entity, &Collider, &Transform, &Sprite)>,
+    ){
+
+    if let Ok((mut ball, ball_transform, ball_sprite)) = ball_query.single_mut(){
+        let ball_size = ball_sprite.size;
+        let velocity = &mut ball.velocity;
+
+        for(collider_entity, collider, transform, sprite) in collider_query.iter(){
+
+            let collision = collide(
+                ball_transform.translation,
+                ball_size,
+                transform.translation,
+                sprite.size,
+                );
+
+            if let Some(collision) = collision{
+
+                //if hits block
+                if *collider == Collider::Break{
+                   commands.entity(collider_entity).despawn(); 
+                }
+
+
+                //reflect ball
+                let mut reflect_x = false;
+                let mut reflect_y = false;
+
+                match collision{
+                    Collision::Left => reflect_x = velocity.x > 0.0,
+                    Collision::Right => reflect_x = velocity.x < 0.0,
+                    Collision::Top => reflect_y = velocity.y < 0.0,
+                    Collision::Bottom => reflect_y = velocity.y > 0.0,
+                }
+
+                if reflect_x{
+                    velocity.x = -velocity.x;
+                }
+
+                if reflect_y{
+                    velocity.y = -velocity.y;
+                }
+
+                if *collider == Collider::Solid{
+                    break;
+                }
+
+                if *collider == Collider::Break{
+                    //turn it into falling block with Vec3 position.
+                    commands
+                        .spawn()
+                        .insert(FallingToSpawn(transform.translation.clone()));
+                }
+            }
+        }
+    }
+}
+
+
     
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use bevy::{
     prelude::*,
-    sprite::collide_aabb::{collide, Collision},
 };
 
 mod ball;
@@ -63,7 +62,6 @@ fn main() {
         .add_plugin(BallPlugin)
         .add_plugin(BlockPlugin)
         .add_plugin(FallingPlugin)
-        .add_system(ball_collision_system.system())
         .run();
 }
 
@@ -139,65 +137,3 @@ fn spawn_walls(
         })
         .insert(Collider::Solid);
 }
-
-fn ball_collision_system(
-    mut commands: Commands,
-    mut ball_query: Query<(&mut Ball, &Transform, &Sprite)>,
-    collider_query: Query<(Entity, &Collider, &Transform, &Sprite)>,
-    ){
-
-    if let Ok((mut ball, ball_transform, ball_sprite)) = ball_query.single_mut(){
-        let ball_size = ball_sprite.size;
-        let velocity = &mut ball.velocity;
-
-        for(collider_entity, collider, transform, sprite) in collider_query.iter(){
-
-            let collision = collide(
-                ball_transform.translation,
-                ball_size,
-                transform.translation,
-                sprite.size,
-                );
-
-            if let Some(collision) = collision{
-
-                //if hits block
-                if *collider == Collider::Break{
-                   commands.entity(collider_entity).despawn(); 
-                }
-
-
-                //reflect ball
-                let mut reflect_x = false;
-                let mut reflect_y = false;
-
-                match collision{
-                    Collision::Left => reflect_x = velocity.x > 0.0,
-                    Collision::Right => reflect_x = velocity.x < 0.0,
-                    Collision::Top => reflect_y = velocity.y < 0.0,
-                    Collision::Bottom => reflect_y = velocity.y > 0.0,
-                }
-
-                if reflect_x{
-                    velocity.x = -velocity.x;
-                }
-
-                if reflect_y{
-                    velocity.y = -velocity.y;
-                }
-
-                if *collider == Collider::Solid{
-                    break;
-                }
-
-                if *collider == Collider::Break{
-                    //turn it into falling block with Vec3 position.
-                    commands
-                        .spawn()
-                        .insert(FallingToSpawn(transform.translation.clone()));
-                }
-            }
-        }
-    }
-}
-
