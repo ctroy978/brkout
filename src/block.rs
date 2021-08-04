46c18bf6113a7d314fa5bf639b4a79a0478a6060
ctroy978 <ctroy978@gmail.com>
2021-08-04 10:37:37 -0700

    rows and col implemented for blocks

--- src/block.rs
+++ src/block.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 
 
-use crate::{Materials, Collider, BLK_HEIGHT, BLK_WIDTH};
+use crate::{Materials, WinSize, Collider, WALL_THICK};
 
 pub struct BlockPlugin;
 
@@ -18,15 +18,37 @@ impl Plugin for BlockPlugin{
 fn block_spawn(
     mut commands: Commands,
     materials: Res<Materials>,
+    winsize: Res<WinSize>,
     ){
-    commands.spawn_bundle(SpriteBundle{
-        material: materials.red_block.clone(),
-        sprite: Sprite::new(Vec2::new(BLK_WIDTH, BLK_HEIGHT)),  
-        transform: Transform{
-            translation: Vec3::new(0.0, 100.0, 0.0),
-            ..Default::default()
-        },
-        ..Default::default()
-    })
-    .insert(Collider::Break);
+    let playable_area = winsize.w - ((WALL_THICK) * 2.0); 
+    let brick_rows = 4;
+    let brick_cols = 7;
+    let brick_spacing = 7.0;
+    let brick_width = playable_area / brick_cols as f32 - brick_spacing;
+    let brick_size = Vec2::new(brick_width, 30.0);
+    let brick_offset = Vec3::new(-(playable_area - brick_size.x - brick_spacing) / 
+                                 2.0, (brick_size.y + brick_spacing) * brick_rows as f32 , 0.0);
+    for row in 0..brick_rows{
+        let y_pos = row as f32 * (brick_size.y + brick_spacing);
+        for col in 0..brick_cols{
+            let brick_pos = Vec3::new(
+                col as f32 * (brick_size.x + brick_spacing),
+                y_pos,
+                10.0,
+                 ) + brick_offset; 
+            //lay bricks
+            commands
+                .spawn_bundle(SpriteBundle{
+                    material: materials.red_block.clone(),
+                    sprite: Sprite::new(brick_size),
+                    transform: Transform::from_translation(brick_pos),
+                    ..Default::default()
+                })
+                .insert(Collider::Break);
+        }
+    }
+    
+
 }
+
+
